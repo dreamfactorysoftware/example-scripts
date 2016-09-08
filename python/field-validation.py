e0f6abbfead4a7bcca3a750addeffad257cc6fb9
Andrew <bg_zero_x12@yahoo.com>
2016-09-08 16:58:53 -0500

    Fix formatting

--- python/field-validation.py
+++ python/field-validation.py
@@ -4,8 +4,8 @@
 payload = event.request.payload;
  
 if(payload.resource):
-  for record in payload.resource:
-    if 'annual_revenue' not in record:
-      raise ValueError('Missing field annual revenue');
-    if record.annual_revenue <= 0:
-      raise ValueError('Annual Revenue must be > 0');
+    for record in payload.resource:
+        if 'annual_revenue' not in record:
+            raise ValueError('Missing field annual revenue');
+        if record.annual_revenue <= 0:
+            raise ValueError('Annual Revenue must be > 0');
