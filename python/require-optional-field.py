99495ef8d80d2d5d5dceba74a6efe5a3d97af683
Andrew <bg_zero_x12@yahoo.com>
2016-09-08 17:34:33 -0500

    Fix require optional field script
    user.register.post does not have a 'resource' object. Work with the payload directly.

--- python/require-optional-field.py
+++ python/require-optional-field.py
@@ -1,11 +1,10 @@
 # POST /api/v2/user/register triggers script user.register.post.pre_process
 # This script runs BEFORE user is registered
- 
-payload = event.request.payload;
- 
-if(payload.resource):
-  for record in payload.resource:
+
+payload = event.request.payload
+
+if(payload):
     if 'first_name' not in record:
-      raise ValueError('First name field missing')
-    if record.first_name == '':
-      raise ValueError('First name field missing')
+        raise ValueError('First name field missing')
+    if payload.first_name == '':
+        raise ValueError("First name field required")
