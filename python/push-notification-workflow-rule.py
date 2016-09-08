8266a38b4219d4d739c6e3368d1f35a815dc798b
Andrew <bg_zero_x12@yahoo.com>
2016-09-08 16:54:31 -0500

    Create push-notification-workflow-rule.py

---
+++ python/push-notification-workflow-rule.py
@@ -0,0 +1,14 @@
+# POST /api/v2/db/_table/todo triggers script db._table.todo.post.post_process
+# This script runs AFTER records are written to the db.
+ 
+payload = event.request.payload;
+ 
+if payload.resource != "":
+  for record in payload.resource:
+    msg = '{"Message":"A new Todo named '+record.name+' was just created!", "Subject":"New Todo Created"}';
+ 
+    # service name is 'push', push to SNS topic by name
+    result = platform.api.post('/push/topic/arn:aws:sns:us-east-1:xxxxxxx:new_todo', msg);
+ 
+    # output result to storage/logs/dreamfactory.log
+    print result.read();
