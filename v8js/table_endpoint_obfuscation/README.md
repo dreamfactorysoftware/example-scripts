d228ee426a00a3209a4184f1c066cc36ea343b81
drewpearce <drewpearce@users.noreply.github.com>
2016-12-08 11:32:06 -0500

    New Table Endpoint Obfuscation Script (#8)
    New script for obfuscating table endpoints. Has a pre 2.4 script and a
    current script.

---
+++ v8js/table_endpoint_obfuscation/README.md
@@ -0,0 +1,11 @@
+This script allows you to obfuscate sql database table endpoints. For example, you might want to change 'absurdlylongdbname/_table/mycustinfo2015' to just 'customers'.
+
+There are two versions. One that works with DreamFactory 2.3.1 and earlier, and one that works with DreamFactory 2.4.0 and later.
+
+To use this script, add your sql database to dreamfactory (if not done already.)
+
+Then create a new script service of type v8js. Name it something short and sweet (like customers in the example above.)
+
+Paste the script into the script editor and change api_path variable to be whatever service/_table/tablename you want to obfuscate.
+Save the service. It is now available using the standard DreamFactory table record API procedures, except the endpoint is shortened.
+See http://wiki.dreamfactory.com/DreamFactory/Features/Database#Table_Records_.28_table.29 for details on Table Record operations.
---
+++ v8js/table_endpoint_obfuscation/table_endpoint_obfuscation_pre_2.4.js
@@ -0,0 +1,44 @@
+var api_path = 'db/_table/todo'; //the service/_table/tablename you wish to obfuscate
+var method = event.request.method;  //get the HTTP Method
+var parameters = event.request.parameters; //copy params from the request to a var
+var result; //create the result var (to be returned by the script)
+
+if (event.resource && event.resource !== '') { //if there are additional resources in the request path add them to our request path
+    api_path = api_path + '/' + event.resource;
+}
+
+if (event.request.payload) { //if the payload is not empty assign it to the payload var
+    var payload = event.request.payload;
+} else { //else make the payload null
+    var payload = null;
+}
+
+if (Object.keys(parameters).length > 0) { //checks to see if the parameters object is not empty
+    api_path = api_path + '?'; //adds the parameters back in to the api call.
+    for (var property in parameters) {
+        api_path = api_path + property + '=' + parameters[property] + '&';
+    }
+}
+
+switch (method) {
+    case 'GET':
+        result = platform.api.get(api_path, payload);
+        break;
+    case 'POST':
+        result = platform.api.post(api_path, payload);
+        break;
+    case 'PUT':
+        result = platform.api.put(api_path, payload);
+        break;
+    case 'PATCH':
+        result = platform.api.patch(api_path, payload);
+        break;
+    case 'DELETE':
+        result = platform.api.delete(api_path, payload);
+        break;
+    default:
+        result = {"message":"Cannot interpret this call. Invalid verb."};
+        break;
+}
+
+return result;
